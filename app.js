9150633484961e6a3dfefac2a901b9bb9bfb0f9b
Schifer <oaarruda@inf.ufrgs.br>
2019-02-05 00:01:47 -0200

    new hacker rank challenges

--- app.js
+++ app.js
@@ -16,11 +16,22 @@ let scores, roundScore, activePlayer, dice;
 scores = [0, 0];
 roundScore = 0;
 activePlayer = 0; // 0 = first player, 1 = second player
-dice = Math.floor(Math.random() * 6) + 1;
+
 
 // DOM Manipulation
 document.querySelector('#current-' + activePlayer).textContent = dice;
+document.querySelector('.dice').style.display = 'none';
+
+document.querySelector('score-0').textContent = '0';
+document.querySelector('score-1').textContent = '0';
+document.querySelector('current-0').textContent = '0';
+document.querySelector('current-1').textContent = '0';
+
 document.querySelector('.btn-roll').addEventListener('click', function(){
-    
+    dice = Math.floor(Math.random() * 6) + 1;
+    let diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-' + dice + '.png';
 });
+
 //document.querySelector('.dice').style.display = 'none';
